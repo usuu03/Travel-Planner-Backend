8afdd79485cbe760bfe8d6ff7a21d3051e13b286
Usu <usuedeaghe@gmail.com>
2023-07-31 19:27:03 +0100

    Initial commit

---
+++ src/main/java/com/travelplanner/travelplanner/models/Destination.java
@@ -0,0 +1,14 @@
+package com.travelplanner.travelplanner.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "destinations")
+public class Destination {
+
+    private Long id;
+    private String name;
+
+    private
+}
